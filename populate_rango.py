12a1a59b11d7ae402c92e09d6f0dd3bdcc166055
“Alexandra <2550454m@student.gla.ac.uk>
2022-02-06 22:13:20 +0000

    Chapter 5: Tests Passed

--- populate_rango.py
+++ populate_rango.py
@@ -34,9 +34,9 @@ def populate():
         {'title': 'Flask',
         'url': 'http://flask.pocoo.org'}]
 
-    cats = {'Python': {'pages': python_pages},
-        'Django': {'pages': django_pages},
-        'Other Frameworks': {'pages': other_pages}}
+    cats = {'Python': {'pages': python_pages, 'likes': 64, 'views': 128},
+        'Django': {'pages': django_pages, 'likes': 32, 'views': 64},
+        'Other Frameworks': {'pages': other_pages, 'likes': 16, 'views': 32}}
 
     # If you want to add more categories or pages,
     # add them to the dictionaries above.
@@ -45,7 +45,7 @@ def populate():
     # and then adds all the associated pages for that category.
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['likes'], cat_data['views'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
@@ -61,8 +61,8 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, likes, views):
+    c = Category.objects.get_or_create(name=name, likes=likes, views=views)[0]
     c.save()
     return c
 
