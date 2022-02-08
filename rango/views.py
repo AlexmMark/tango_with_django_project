3885149ed8a56a7ae09ee70f3c751b580ba568a4
“Alexandra <2550454m@student.gla.ac.uk>
2022-02-08 11:57:42 +0000

    Chapter 7.2

--- rango/views.py
+++ rango/views.py
@@ -4,18 +4,21 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category, Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
+
 
 def index(request):
-
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
-    context_dict={}
-    #dictionary for {{boldmessage}} template
+    context_dict = {}
+    # dictionary for {{boldmessage}} template
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
     return render(request, 'rango/index.html', context=context_dict)
+
 
 def about(request):
     # dictionary for {{boldmessage}} template
@@ -23,8 +26,8 @@ def about(request):
 
     return render(request, 'rango/about.html', context=context_dict)
 
-def show_category(request, category_name_slug):
 
+def show_category(request, category_name_slug):
     context_dict = {}
 
     try:
@@ -32,8 +35,8 @@ def show_category(request, category_name_slug):
 
         pages = Page.objects.filter(category=category)
 
-        context_dict['pages']=pages
-        context_dict['category']=category
+        context_dict['pages'] = pages
+        context_dict['category'] = category
 
     except Category.DoesNotExist:
         context_dict['category'] = None
@@ -42,3 +45,25 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context=context_dict)
 
 
+def add_category(request):
+    form = CategoryForm()
+
+    # A HTTP POST?
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+    # Have we been provided with a valid form?
+
+    if form.is_valid():
+        # Save the new category to the database.
+        form.save(commit=True)
+        # Now that the category is saved, we could confirm this.
+        # For now, just redirect the user back to the index view.
+        return redirect('/rango/')
+    else:
+        # The supplied form contained errors -
+        # just print them to the terminal.
+        print(form.errors)
+
+    # Will handle the bad form, new form, or no form supplied cases.
+    # Render the form with error messages (if any).
+    return render(request, 'rango/add_category.html', {'form': form})
